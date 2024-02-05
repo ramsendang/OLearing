216e9fc52e35b182f894f7c6c05176c957f0008f
ramsendang <ramsendang02@gmail.com>
2024-02-05 15:16:00 +0000

    removed search bar from the navigation menu

--- frontend/component/header.php
+++ frontend/component/header.php
@@ -27,9 +27,6 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Categories
@@ -50,10 +47,6 @@
                                     
                                 </ul>
                             </li>
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
                         </ul>
                         <div class="dropdown">
                             <button class="btn btn-secondary dropdown-toggle bord" type="button" data-bs-toggle="dropdown" aria-expanded="false">
